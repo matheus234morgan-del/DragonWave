d43f9a4fccc2a51989f320bebf53d944c082644b
matheus234morgan-del <matheus234morgan@gmail.com>
2026-05-23 10:02:58 -0300

    Update Main.lua

--- Scripts/Main.lua
+++ Scripts/Main.lua
@@ -1,1 +1,9 @@
 
+local UI = require(script.Parent.UI)
+local LockOn = require(script.Parent.LockOn)
+local Combat = require(script.Parent.Combat)
+
+-- START UI
+UI:Create()
+
+print("DragonWave Loaded")
