98d2883a88b2ac611d678e035affdae2567a9468
matheus234morgan-del <matheus234morgan@gmail.com>
2026-05-23 10:12:32 -0300

    Update DragonWave.lua

--- DragonWave.lua
+++ DragonWave.lua
@@ -1,1 +1,14 @@
 
+local Config = {}
+
+-- LOCK SETTINGS
+Config.LockDistance = 150
+Config.CameraDistance = 8
+Config.CameraHeight = 4
+Config.Smoothness = 0.15
+
+-- UI SETTINGS
+Config.ButtonText = "LOCK ON"
+Config.UnlockText = "UNLOCK"
+
+return Config
